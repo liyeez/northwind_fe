85f88e44788503c6173dad609c217aac662efa6d
Liyee Zhen <liyee.zhen@ventia.com>
2021-05-26 06:02:11 +0000

    setting up destinations

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -4,6 +4,11 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("sap.btp.sapui5.controller.List",{
+        onInit: function () {
+            this.serviceURL = this.getOwnerComponent().getManifestEntry("/sap.app/dataSources/HANA/uri");
+            console.log(this.serviceURL);
+        },
+
         handleListItemPress: function (oEvent) {
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this); 
             //get router object
